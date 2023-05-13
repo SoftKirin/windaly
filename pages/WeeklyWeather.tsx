9bd1402ef4856b68f906c0f9101c084a377b4d59
Andy c <81699558+andycr123@users.noreply.github.com>
2023-05-13 10:55:45 -0500

    changes

--- pages/WeeklyWeather.tsx
+++ pages/WeeklyWeather.tsx
@@ -1,11 +1,35 @@
 import React from 'react'
 
-const WeeklyWeather = () => {
+const WeeklyWeather = (/*{weeklyweather, WeeklyWeather, timezone }*/) => {
   return (
-    <div>
-      <h1>Weekly Weather component</h1>
+    <div className='weekly'>
+     <h3 className="weekly__tittle">
+        Weekly <span>Weather</span>
+     </h3>
+
+        {WeeklyWeather.length > 0  && 
+            weeklyWeather.map((weather, index)=> {
+                if(index ==0) {
+                    return;
+                }
+                return(
+                    <div className="weekly__card" key={weather.dt}>
+                        <div className="weekly__inner">
+                            <div className="weekly__left-content">
+                                <div>
+                                    <h3>
+                                        
+                                    </h3>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                );
+            })
+        }
+
     </div>
   )
 }
 
-export default WeeklyWeather
+export default WeeklyWeather;
